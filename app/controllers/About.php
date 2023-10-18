d05e187d0ddf4c957cc18d1d53eaf13d5ec42ee4
rejka permana <permanarejka@gmail.com>
2023-10-18 17:54:33 +0700

    Video 5 - Views

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,10 +1,19 @@
 <?php
 
 class About {
-    public function index($nama = "Rejka", $pekerjaan="Ruby on Rails developer") {
-        echo "Halo nama saya $nama, Saya seorang $pekerjaan";
-    }
+    public function index($nama = "Rejka", $pekerjaan="Ruby on Rails developer", $umur=23) {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+      }
     public function page() {
-        echo 'About/Page';
-    }
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+      }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,4 @@
+<h1>About me</h1>
+    <p>Hallo, Nama saya <?= $data['nama'];?> umur saya <?= $data['umur'];?>, saya adalah <?= $data['pekerjaan'];?></p>
+</body>
+</html>
